81a75af209af576592bd985439db3681a2c06097
Max <bigin.maks@gmail.com>
2021-08-24 16:02:05 +0300

    correct generate

--- src/js/generate.js
+++ src/js/generate.js
@@ -1,7 +1,8 @@
 function loadFile(url, callback) {
   PizZipUtils.getBinaryContent(url, callback);
 }
-function Generate(url) {
+
+function GenerateS(url) {
   loadFile(url, function (error, content) {
     if (error) {
       throw error;
@@ -58,6 +59,67 @@ function Generate(url) {
       mimeType:
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
     });
-    saveAs(out, "output.docx");
+    saveAs(out, "sluzhebnaya-zapiska.docx");
+  });
+}
+
+function GenerateR(url) {
+  loadFile(url, function (error, content) {
+    if (error) {
+      throw error;
+    }
+
+    function replaceErrors(key, value) {
+      if (value instanceof Error) {
+        return Object.getOwnPropertyNames(value).reduce(function (error, key) {
+          error[key] = value[key];
+          return error;
+        }, {});
+      }
+      return value;
+    }
+    function errorHandler(error) {
+      console.log(JSON.stringify({ error: error }, replaceErrors));
+
+      if (error.properties && error.properties.errors instanceof Array) {
+        const errorMessages = error.properties.errors
+          .map(function (error) {
+            return error.properties.explanation;
+          })
+          .join("\n");
+        console.log("errorMessages", errorMessages);
+      }
+      throw error;
+    }
+
+    var zip = new PizZip(content);
+    var doc;
+    try {
+      doc = new window.docxtemplater(zip, {
+        paragraphLoop: true,
+        linebreaks: true,
+      });
+    } catch (error) {
+      errorHandler(error);
+    }
+
+    doc.setData({
+      tutorial: "Программирование Go vs Python",
+      direction: "MIT",
+      date: "11.11.2011",
+      authors: "Bigin Maxim Sergeevich",
+    });
+    try {
+      doc.render();
+    } catch (error) {
+      errorHandler(error);
+    }
+
+    var out = doc.getZip().generate({
+      type: "blob",
+      mimeType:
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    });
+    saveAs(out, "zayavka-na-razmeshchenie.docx");
   });
 }
